3c4aea3c29abf1b9d595837bbb47cec539c4c020
Princess M <princessmosse@gmail.com>
2021-07-05 16:53:49 -0400

    updating landing page details

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{height: '300px', position: 'relative'}}>
-    <Layout fixedHeader>
-        <Header title="Princess M" style={{color: 'white'}} scroll>
-            <Navigation>
-                <a href="/">About Me</a>
-                <a href="/">Resume</a>
-                <a href="/">Projects</a>
-                <a href="/">Contact</a>
+    <div className="demo-big-content">
+    <Layout>
+    <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Princess M Portfolio</Link>} scroll>       
+     <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <a href="/">About Me</a>
-                <a href="/">Resume</a>
-                <a href="/">Projects</a>
-                <a href="/">Contact</a>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Menu</Link>}>           
+         <Navigation>
+            <Link to="/aboutme">About Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
       return(
-          <div>
-              <h1>Landing Page</h1>
-          </div>
+          <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://i.pinimg.com/564x/9b/c8/a7/9bc8a754d4e2c30f485b3e182df03196.jpg"
+              alt="avatar"
+              className="avatar-img"
+              />
+
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+            <hr/>
+
+          <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express</p>
+
+        <div className="social-links">
+
+          {/* LinkedIn */}
+          <a href="https://www.linkedin.com/in/princess-moss-8b290a140/" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-linkedin-square" aria-hidden="true" />
+          </a>
+
+          {/* Github */}
+          <a href="https://github.com/princessmoss" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+
+          {/* Email */}
+          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+          </a>
+
+        </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
       )
     }
 }

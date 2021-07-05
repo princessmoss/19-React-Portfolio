231197a40be98e52ea81f0e9f45eb5ca05c79e04
Princess M <princessmosse@gmail.com>
2021-07-05 15:43:41 -0400

    adding components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
+import Main from './components/main';
 
 function App() {
   return (
     <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(https://i.pinimg.com/originals/62/93/ff/6293ff11ecbc1f92dac97ab5970f4d71.webp) center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
+    <Layout fixedHeader>
+        <Header title="Princess M" style={{color: 'white'}} scroll>
             <Navigation>
                 <a href="/">About Me</a>
                 <a href="/">Resume</a>
-                <a href="/">Portfolio</a>
+                <a href="/">Projects</a>
                 <a href="/">Contact</a>
             </Navigation>
         </Header>
@@ -17,13 +19,37 @@ function App() {
             <Navigation>
                 <a href="/">About Me</a>
                 <a href="/">Resume</a>
-                <a href="/">Portfolio</a>
+                <a href="/">Projects</a>
                 <a href="/">Contact</a>
             </Navigation>
         </Drawer>
-        <Content />
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
     </Layout>
 </div>
+//     <div style={{height: '300px', position: 'relative'}}>
+//     <Layout style={{background: 'url(https://i.pinimg.com/originals/62/93/ff/6293ff11ecbc1f92dac97ab5970f4d71.webp) center / cover'}}>
+//         <Header transparent title="Princess M." style={{color: 'white'}}>
+//             <Navigation >
+//                 <a href="/" style={{color: 'black'}}>About Me</a>
+//                 <a href="/" style={{color: 'black'}}>Resume</a>
+//                 <a href="/" style={{color: 'black'}}>Portfolio</a>
+//                 <a href="/" style={{color: 'black'}}>Contact</a>
+//             </Navigation>
+//         </Header>
+//         <Drawer title="Menu" >
+//             <Navigation>
+//                 <a href="/">About Me</a>
+//                 <a href="/">Resume</a>
+//                 <a href="/">Portfolio</a>
+//                 <a href="/">Contact</a>
+//             </Navigation>
+//         </Drawer>
+//         <Content />
+//     </Layout>
+// </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+  <App />
+    </BrowserRouter> 
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

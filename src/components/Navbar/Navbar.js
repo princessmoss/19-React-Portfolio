23f0404b606fa4b35039eef80a0605be7d63dee9
Princess M <princessmosse@gmail.com>
2021-07-11 23:34:29 -0400

    creating navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,20 @@
+import format from './Navbar.module.css'
+
 const Navbar = () => {
     return (
-      <div>
+      <div className = {format.Navbar}>
         <nav>
           <ul>
             <li>
-            <a href="/">Home</a>
-              <a href="/">About Me</a>
+            <a href="/" >Home</a>
+            </li>
+            <li>
+              <a href="/" >About Me</a>
+            </li>
+            <li>
                <a href="/">Projects</a>
+               </li>
+               <li>
                <a href="/">Contact</a>
             </li>
           </ul>
